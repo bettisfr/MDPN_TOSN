231ca145809acc2bbf35c531ce009d3ff199a3f3
Francesco Betti Sorbelli <francesco.betti.sorbelli@gmail.com>
2023-11-13 10:47:49 +0100

    implementation

--- src/input.h
+++ src/input.h
@@ -5,6 +5,8 @@
 #include <ctime>
 #include <cstdlib>
 #include <map>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -20,20 +22,104 @@ struct input {
     string exp_name = "default";
 
     // Number of sensors
-    int num_sensors;
+    int num_sensors = 100;
 
     // Length of the area (in meters)
-    int area_length;
+    int area_length = 1000;
 
     // Width of the area (in meters)
-    int area_width;
+    int area_width = 1000;
 
     // Radius of the sensors (in case of no DOI)
-    double sensor_radius;
+    double sensor_radius = 150;
 
     // Degree of Irregularity (DOI)
-    double doi;
+    // https://www.sciencedirect.com/science/article/pii/S1574119218305406 (Section 4)
+    // DOI=0 sphere, DOI>0 irregularities
+    double doi = 0;
+
+    // Number of random scenarios
+    int scenarios = 10;
 
 };
+
+map<int, string> exp_str = {
+    {0, "Default parameters"},
+    {1, "From loaded parameters"},
+};
+
+void print_parameters(const input &);
+void save_parameters(const input &);
+input load_parameters(input &);
+
+void print_parameters(const input &par) {
+    cout << "Seed=" << par.seed << endl;
+    cout << "Experiment name=" << par.exp_name << endl;
+    cout << "Number of scenarios=" << par.scenarios << endl;
+
+    cout << "Experiment=" << par.experiment << " (" << exp_str[par.experiment] << ")" << endl << endl;
+
+    cout << "Area length=" << par.area_length << "m" << endl;
+    cout << "Area width=" << par.area_width << "m" << endl;
+    cout << "Number of sensors=" << par.num_sensors << endl;
+    cout << "Radius of sensor=" << par.sensor_radius << endl;
+    cout << "DOI=" << par.doi << endl;
+
+    cout << endl << endl;
+}
+
+void save_parameters(const input &par) {
+    string cfg_filename = "input/" + par.exp_name + ".cfg";
+    ofstream file_cfg(cfg_filename);
+
+    file_cfg << "seed=" << par.seed << endl;
+    file_cfg << "area_length=" << par.area_length << endl;
+    file_cfg << "area_width=" << par.area_width << endl;
+    file_cfg << "num_sensors=" << par.num_sensors << endl;
+    file_cfg << "sensor_radius=" << par.sensor_radius << endl;
+    file_cfg << "doi=" << par.doi << endl;
+    file_cfg << "scenarios=" << par.scenarios << endl;
+
+    file_cfg << endl;
+
+    file_cfg.close();
+}
+
+input load_parameters(input &par) {
+    string cfg_filename = "input/" + par.exp_name + ".cfg";
+    ifstream file_cfg(cfg_filename);
+
+    string line;
+    while (getline(file_cfg, line)) {
+        stringstream lineStream(line);
+        string key;
+        string value;
+
+        if (getline(lineStream, key, '=') && lineStream >> value) {
+            if (key == "seed") {
+                par.seed = stoi(value);
+            } else if (key == "area_length") {
+                par.area_length = stoi(value);
+            } else if (key == "area_width") {
+                par.area_width = stoi(value);
+            } else if (key == "num_sensors") {
+                par.num_sensors = stoi(value);
+            } else if (key == "sensor_radius") {
+                par.sensor_radius = stod(value);
+            } else if (key == "doi") {
+                par.doi = stod(value);
+            } else if (key == "scenarios") {
+                par.scenarios = stoi(value);
+            }
+        }
+    }
+
+    file_cfg.close();
+
+    return par;
+}
+
+
+
 
 #endif //TOSN_INPUT_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,46 @@
 #include <iostream>
 
+#include "input.h"
 #include "core/sensor.h"
 #include "core/deployment.h"
 
 using namespace std;
 
+void run_experiment(input &par) {
+//    int n = 100;
+//    int length = 1000;
+//    int width = 1000;
+//    double radius = 150;
+//    double doi = 1;
+//
+//    deployment dep(n, length, width, radius, doi);
+
+    if (par.experiment != 0) {
+        par = load_parameters(par);
+    }
+
+    // Print used parameters
+    print_parameters(par);
+}
+
 int main(int argc, char **argv) {
 
-    int n = 100;
-    int length = 1000;
-    int width = 1000;
-    double radius = 150;
-    double doi = 1;
+    input par;
 
-    deployment dep(n, length, width, radius, doi);
+    if (argc >= 3) {
+        par.experiment = atoi(argv[1]);
+        par.exp_name = argv[2];
+    } else {
+        cerr << "Error. Use: executable 0|1 filename" << endl;
+        exit(1);
+    }
+
+    if (par.experiment != 0 && par.experiment != 1) {
+        cerr << "\n[ERROR main] experiment to run not present\n";
+        exit(1);
+    }
+
+    run_experiment(par);
 
     return 0;
 }
