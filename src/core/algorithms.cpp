ffc6c6c748d0cf58cf9f9910d9b97048fc728b19
Francesco Betti Sorbelli <francesco.betti.sorbelli@gmail.com>
2023-11-15 12:17:18 +0100

    intersection

--- src/core/algorithms.cpp
+++ src/core/algorithms.cpp
@@ -71,6 +71,13 @@ bool algorithms::is_within_radius_doi(const sensor& s, point p) {
 void algorithms::tsp_neighbors() {
     // sort sensors from left to right
 
+    point p1 = {0, 0};
+    point p2 = {10, 10};
+    vector<point> aaa = get_intersection_points(p1, p2);
+    for (point p : aaa) {
+        cout << get<0>(p) << ", " << get<1>(p) << endl;
+    }
+    cout << endl;
 
     // create independent set
 
@@ -99,21 +106,49 @@ void algorithms::tsp_neighbors() {
     cout << "TSP len: " << len << endl;
 }
 
-vector<point> algorithms::get_intersection_points(point a, point b) {
+vector<point> algorithms::get_intersection_points(point pa, point pb) {
     // given two points a and b, returns the intersection points
     point int_ab_1 = {-1, -1};
     point int_ab_2 = {-1, -1};
-
-    double x1 = get<0>(a);
-    double y1 = get<1>(a);
-    double x2 = get<0>(b);
-    double y2 = get<1>(b);
-
-    // TODO ...
-    int_ab_1 = {4, 5};
-    int_ab_2 = {24, 15};
-
     vector<point> int_points;
+
+    double x1 = get<0>(pa);
+    double y1 = get<1>(pa);
+    double x2 = get<0>(pb);
+    double y2 = get<1>(pb);
+
+    // Calculate distance between the centers of the circles
+    double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+
+    // Check if circles are too far apart or coincide
+    if (distance > 2 * sensor_radius || distance < fabs(sensor_radius - sensor_radius)) {
+//        cout << "No intersection points found." << endl;
+        return int_points;
+    }
+
+    // Calculate the distance from the center of circle A to the line joining the intersection points
+    double a = (pow(sensor_radius, 2) - pow(sensor_radius, 2) + pow(distance, 2)) / (2 * distance);
+
+    // Calculate the coordinates of the intersection points
+    double x3 = x1 + a * (x2 - x1) / distance;
+    double y3 = y1 + a * (y2 - y1) / distance;
+
+    double h = sqrt(pow(sensor_radius, 2) - pow(a, 2));
+
+    // Calculate the coordinates of the two intersection points
+    double int_x1 = x3 + h * (y2 - y1) / distance;
+    double int_y1 = y3 - h * (x2 - x1) / distance;
+
+    double int_x2 = x3 - h * (y2 - y1) / distance;
+    double int_y2 = y3 + h * (x2 - x1) / distance;
+
+    // Output the intersection points
+//    cout << "Intersection Point 1: (" << int_x1 << ", " << int_y1 << ")" << endl;
+//    cout << "Intersection Point 2: (" << int_x2 << ", " << int_y2 << ")" << endl;
+
+    int_ab_1 = {int_x1, int_y1};
+    int_ab_2 = {int_x2, int_y2};
+
     int_points.push_back(int_ab_1);
     int_points.push_back(int_ab_2);
 
